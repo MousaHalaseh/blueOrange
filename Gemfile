9964f7b5e7be33a069be83a84ef0d85fa5dd2bc0
Mousa Halaseh <mousa@bluekangaroo.co>
2018-06-06 13:19:09 +0300

    User auth has been added

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.4.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
+# Use Bcrypt as the authintecation service for the application
+gem 'bcrypt', '~> 3.1.12'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,7 @@ class UserTest < ActiveSupport::TestCase
 
 
   def setup
-    @user = User.new(name: "Mousa Halaseh", :email => "mousahalaseh@outlook.com")
+    @user = User.new(name: "Mousa Halaseh", :email => "mousahalaseh@outlook.com", password: "mousa#something", password_confirmation: "mousa#something")
   end
 
   test "user should be valid" do
@@ -41,4 +41,29 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "This email has been accepted: #{invalid_address.inspect}. it should be invalid though"
     end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
